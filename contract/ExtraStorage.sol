4f01409bc63fe122776614b133c74c517a3971d2
zhengtianl <458078290@qq.com>
2024-09-07 16:57:22 +0800

    change

---
+++ contract/ExtraStorage.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+import "./SimpleStorage.sol";
+contract ExtraStorage is SimpleStorage{
+    function store(uint256 _favoriteNumber) public override {
+        favoriteNumber = _favoriteNumber + 10;
+    }
+}
